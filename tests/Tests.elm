1f818362d0b4d02e8424e44034a960f835461d69
abab <ababup1192@gmail.com>
2018-11-13 21:25:58 +0900

    [Refactor] Test case

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,30 +6,36 @@ import Main exposing (..)
 import Test exposing (..)
 
 
+type alias TestCase =
+    String
+
+
+element2SelectedElementViewModelTest : TestCase -> Element -> SelectedElementViewModel -> Test
+element2SelectedElementViewModelTest testCase element selectedElementViewModel =
+    test testCase <|
+        \_ ->
+            let
+                actual =
+                    element2SelectedElementViewModel element
+
+                expected =
+                    selectedElementViewModel
+            in
+            Expect.equal actual expected
+
+
 suite : Test
 suite =
     describe "The Main module"
         [ describe "element2SelectedElementViewModel"
             -- Nest as many descriptions as you like.
-            [ test "火が選択されている！" <|
-                \_ ->
-                    let
-                        actual =
-                            element2SelectedElementViewModel Fire
-
-                        expected =
-                            SelectedElementViewModel " selected" "" "" ""
-                    in
-                    Expect.equal actual expected
-            , test "水が選択されている！" <|
-                \_ ->
-                    let
-                        actual =
-                            element2SelectedElementViewModel Water
-
-                        expected =
-                            SelectedElementViewModel "" " selected" "" ""
-                    in
-                    Expect.equal actual expected
+            [ element2SelectedElementViewModelTest
+                "火が選択されている！"
+                Fire
+                (SelectedElementViewModel " selected" "" "" "")
+            , element2SelectedElementViewModelTest
+                "水が選択されている！"
+                Water
+                (SelectedElementViewModel "" " selected" "" "")
             ]
         ]
