940ac869a99286e58b7e7f510a3aced5b51b05c3
abab <ababup1192@gmail.com>
2018-11-13 21:41:42 +0900

    Add select msg

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (Element(..), SelectedElementViewModel, element2SelectedEle
 import Browser
 import Html exposing (Html, a, div, h1, text)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 
 
@@ -29,13 +30,25 @@ init _ =
 ---- UPDATE ----
 
 
+type OrdinalNum
+    = First
+    | Second
+
+
 type Msg
-    = NoOp
+    = SelectElement OrdinalNum Element
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SelectElement ordinalNum element ->
+            case ordinalNum of
+                First ->
+                    ( { model | firstElement = element }, Cmd.none )
+
+                Second ->
+                    ( { model | secondElement = element }, Cmd.none )
 
 
 
@@ -48,10 +61,10 @@ view { firstElement, secondElement } =
         [ div [ class "elements-container" ]
             [ firstElement
                 |> element2SelectedElementViewModel
-                |> selectedElementViewModel2View
+                |> selectedElementViewModel2View First
             , secondElement
                 |> element2SelectedElementViewModel
-                |> selectedElementViewModel2View
+                |> selectedElementViewModel2View Second
             ]
         , h1 [] [ text "活性" ]
         ]
@@ -77,13 +90,13 @@ element2SelectedElementViewModel element =
             SelectedElementViewModel "" "" "" " selected"
 
 
-selectedElementViewModel2View : SelectedElementViewModel -> Html Msg
-selectedElementViewModel2View { fire, water, wind, ground } =
+selectedElementViewModel2View : OrdinalNum -> SelectedElementViewModel -> Html Msg
+selectedElementViewModel2View ordinalNum { fire, water, wind, ground } =
     div [ class "elements" ]
-        [ a [ class <| "element fire" ++ fire ] [ text "火" ]
-        , a [ class <| "element water" ++ water ] [ text "水" ]
-        , a [ class <| "element wind" ++ wind ] [ text "風" ]
-        , a [ class <| "element ground" ++ ground ] [ text "土" ]
+        [ a [ class <| "element fire" ++ fire, onClick <| SelectElement ordinalNum Fire ] [ text "火" ]
+        , a [ class <| "element water" ++ water, onClick <| SelectElement ordinalNum Water ] [ text "水" ]
+        , a [ class <| "element wind" ++ wind, onClick <| SelectElement ordinalNum Wind ] [ text "風" ]
+        , a [ class <| "element ground" ++ ground, onClick <| SelectElement ordinalNum Ground ] [ text "土" ]
         ]
 
 
